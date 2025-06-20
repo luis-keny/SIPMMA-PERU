9d40136537ae66e2b164272ceddef6d91bd64a84
ErickMendoza21 <erickmendozaojeda14@gmail.com>
2025-06-20 00:45:05 -0500

    feat: add page main

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+
+   menuVisible = true;
+
+  toggleMenu(): void {
+    this.menuVisible = !this.menuVisible;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    const clickedInside = target.closest('.header__group--buttons') || target.closest('.header__logo-icon');
+    if (!clickedInside) {
+      this.menuVisible = false;
+    }
+  }
 }
